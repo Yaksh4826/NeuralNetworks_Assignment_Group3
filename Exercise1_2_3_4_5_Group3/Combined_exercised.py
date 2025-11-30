89ac6ca5980aaa870b4a3e8414d89498046ede7e
Yaksh Patel <yakshpatel4826@gmail.com>
2025-11-30 12:53:11 -0500

    Exercise 3 implementation

--- Exercise1_2_3_4_5_Group3/Combined_exercised.py
+++ Exercise1_2_3_4_5_Group3/Combined_exercised.py
@@ -5,28 +5,26 @@ import numpy as np
 
 
 
-""" Exercise : 1 Generating the training and testing data for the model"""
 
-"""Generating thee training data for the neural network model.
+
+
+
+
+
+""" Exercise : #3 Generating the training and testing data for the model"""
+
+"""Generating the training data for the neural network model.
 
 Generating the training random data using the the uniform method
 
-Setting the size as 10x2 as per  the requirement"""
+Setting the size as 100x2 as per  the requirement   (Generating 100 random instances in 2 sets )"""
 
-nd_Group3  = np.random.uniform(low=-0.6 , size=(10,2), high=0.6);
-
-
-"""Printing the values of the array for the general insight """
-
-print("\n----The training set (input)---\n",nd_Group3.shape);
-print(nd_Group3)
+input_Group_Ex3  = np.random.uniform(low=-0.6 , size=(100,2), high=0.6);
 
 
 """Creating the testing (output) data set for from the training data set"""
+output_Group3_Ex3 = np.sum(input_Group_Ex3 , axis=1).reshape(100,1)
 
 
-
-
-
-
+print(output_Group3_Ex3)
 
